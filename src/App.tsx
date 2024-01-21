a356db5282c8c7c5d38700a31f289a67bd93025c
Ramanjs <raman.s2662@gmail.com>
2024-01-21 18:14:35 +0530

    styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import { connectToMetamask } from './metamaskService'
+import logo from './assets/icon.jpg'
 
 function App() {
   const [connect, setConnect] = useState(false)
@@ -27,20 +28,26 @@ function App() {
   return (
     <>
       <div className='bgimage w-screen h-screen z-[-10]'></div>
-      <div className='w-full min-h-screen flex flex-col justify-between items-center p-8'>
-        <h1 className='font-bold text-4xl text-white'>Hedera Carbon Footprint</h1>
-        <div className='flex flex-col items-center w-[90%] max-w-96 bg-white p-4 rounded-xl shadow-md shadow-blue-400'>
-          <p></p>
-          {!connect && <button className='bg-blue-600 p-4 text-white font-semibold rounded-md' onClick={handleClick}>Connect to Metamask</button>}
-          {connect && (
-            <div className='flex justify-between border-2 border-gray-400'>
-              <p className='p-2'>{numtx * 0.00001} kg CO2</p>
-              <span className='border-l-2 border-gray-400'/>
-              <p className='p-2'>{numtx} Total Transactions</p>
+      <div className='w-full min-h-screen flex flex-col justify-center items-center p-8'>
+        <div className='flex flex-col items-center w-[90%] max-w-[800px] bg-black p-4 rounded-xl shadow-md shadow-blue-400 bg-opacity-75 text-white'>
+          <h1 className='font-bold text-4xl mb-8'>Hedera Carbon Footprint</h1>
+          <div className='w-full flex justify-between'>
+            <img src={logo} className='w-[30%]'/>
+            <div className='flex flex-col justify-between w-[65%]'>
+              <p>Our platform provides a transparent look at the carbon footprint of your blockchain activities, driving forward the mission of sustainable development. Here, you can visualize the environmental impact of your blockchain transactions and take steps towards offsetting your carbon footprint.</p>
+              <p>Make a tangible difference by considering the <a href='https://terrapass.com/' className='underline'>purchase of carbon offsets</a>. By investing in verified environmental projects, you can neutralize your blockchain transactions' carbon footprint.</p>
+              {!connect && <button className='bg-blue-600 p-4 text-white font-semibold rounded-md' onClick={handleClick}>Connect to Metamask</button>}
+              {connect && (
+                <div className='flex justify-between border-2 border-gray-400'>
+                  <p className='p-2'>{numtx * 0.00001} kg CO2</p>
+                  <span className='border-l-2 border-gray-400'/>
+                  <p className='p-2'>{numtx} Total Transactions</p>
+                </div>
+              )}
+              <p className='text-gray-400'>Made by <a href="https://github.com/ramanjs" className='underline'>ramanjs</a> for <a href='https://web3apps-20170.devpost.com/' className='underline'>Web3Apps</a>. Photos from <a href='https://unsplash.com' className='underline'>Unsplash</a>.</p>
             </div>
-          )}
+          </div>
         </div>
-        <div></div>
       </div>
     </>
   )
